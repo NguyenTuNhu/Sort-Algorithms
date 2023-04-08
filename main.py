1ff54d4f5212586ed7fbd9a0e926bc8afb9ee7f6
Nguyen Tu Nhu <129203784+YvesFleur@users.noreply.github.com>
2023-04-08 07:17:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ arr = [2,3,6,8,-6.5,-1]
 print("Input: ", arr)
 
 from bubble_sort import bubblesort
-print("Sorting a list using Bubble sort algorithms: ", bubblesort(arr))
+print("Sorting a list using Bubble sort algorithm: ", bubblesort(arr))
 
 from insertion_sort import insertionsort
-print("Sorting a list using Insertion sort algorithms: ", insertionsort(arr))
+print("Sorting a list using Insertion sort algorithm: ", insertionsort(arr))
 
 from selection_sort import selectionsort
-print("Sorting a list using Selection sort algorithms: ", selectionsort(arr))
+print("Sorting a list using Selection sort algorithm: ", selectionsort(arr))
 
